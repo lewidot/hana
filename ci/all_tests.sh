63a7c2d3fc90b74e86928ce9fdd9997e2d82f665
Lewis Evans <lewisevansuk@gmail.com>
2026-08-20 11:55:05 +0100

    include bundling

--- ci/all_tests.sh
+++ ci/all_tests.sh
@@ -2,6 +2,17 @@
 
 set -euo pipefail
 
+echo "Checking formatting..."
 roc fmt --check package examples
+
+echo "Checking Hana..."
 roc check package/Hana.roc --main=package/main.roc
+
+echo "Checking examples..."
 roc check examples/hello.roc
+
+echo "Bundling Hana..."
+rm -rf dist
+./scripts/bundle.sh
+
+echo "All checks passed."
